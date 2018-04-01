a18e19d274551bb50221d0bd7b2efcaf7592b25e
gopidideepak <37783966+gopidideepak@users.noreply.github.com>
2018-04-01 18:54:28 -0400

    Adjust Authenticator class

--- BudgetPlanner/src/login/security/Authenticator.java
+++ BudgetPlanner/src/login/security/Authenticator.java
@@ -12,17 +12,58 @@ import java.util.Map;
  */
 
 public class Authenticator {
-    private final Map<String, String> USERS = new HashMap<>();
+    private final String DB_URL = "jdbc:derby:BudgetDB;create=true";
+    private final  String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    private final  String TABLE_NAME = "students";
+
+    private Map<String, String> USERS = new HashMap<>();
+    private Map<String, Integer> Quiz = new HashMap();
+    private Map<String, String> Answer = new HashMap<>();
+    private Map<Integer, String> Quizs = new HashMap();
     
     public  boolean validate(String user, String password){
         String validUserPassword = USERS.get(user);
         return validUserPassword != null && validUserPassword.equals(password);
+    }
+    public boolean removeUser(String user){
+        if (USERS.containsKey(user)) {
+            USERS.remove(user);
+            return true;
+        }
+        return false;
+    }
+    public boolean addUser(String user){
+        if (USERS.containsKey(user)) 
+            return false;
+        USERS.put(user, user);
+        return true;
+    }
+    public boolean isExist(String user){
+        
+        return USERS.containsKey(user);
+    }
+    public String getQuiz(String user){
+        String q ="";
+        if( USERS.containsKey(user)) return  Quizs.get(Quiz.get(user));
+        return q;
+    }
+    public String getAnswer(String user){
+        String q ="";
+        if( USERS.containsKey(user)) return  Answer.get(user);
+        return q;
     }
     private static Authenticator instance = null;
    protected Authenticator() {
       // Exists only to defeat instantiation.
       USERS.put("demo", "demo");
       USERS.put("a", "b");
+      Quiz.put("demo",1);
+      Quiz.put("a",2);
+      Answer.put("a", "b1");
+      Answer.put("demo", "demo1");
+      Quizs.put(1, "What's your pet's name?");
+      Quizs.put(2, "In what city were you born?");
+      Quizs.put(3, "What is the name of your first school?");
    }
    public static Authenticator getInstance() {
       if(instance == null) {
